b63bf4a857103b0c16dc96e0b034fec4df11d977
João Vitor Nogueira <jvnogueira2010@gmail.com>
2024-03-10 00:32:32 -0300

    Implementação de prompt para definir o limite de unfollow e também para definir os usernames que vão permanecer seguindo, assim reduzindo a necessidade de alterações diretamente pelo editor de código. Atualização no README.md com imagem de exemplo sobre 'Seguindo' no instagram

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # Instagram Unfollow Bot
 
-Desenvolvimento de bot personalizado para deixar de seguir automaticamente no Instagram, com exceção dos usernames que definir para permanecer seguindo
+Desenvolvimento de bot personalizado para deixar de seguir automaticamente no instagram, com exceção dos usernames que definir para permanecer seguindo e a quantidade de usernames que vai deixar de seguir. Não há necessidade de conhecimento em programação para a realização do download da extensão e utilizar o bot
 
 ## Funcionamento
 
-1. Copiar a pasta do repositório remoto para o repositório local
+1. Copiar a pasta do repositório remoto para o repositório local (git clone https://github.com/jv-nogueira/instagramUnfollowBot.git)
 2. Acessar o seu navegador e clicar em 'Gerenciar extensões'. Habilitar o 'Modo do desenvolvedor' (no meu caso, utilizei o navegador Edge)
 3. Clicar em 'Carregar sem pacote' e abrir a pasta do repositório local
 4. Verificar se a extensão 'instagramUnfollow' está habilitada (conforme a imagem abaixo)
-5. Abrir o Instagram e clicar em 'F5' para atualizar a página. Em seguida, na aba do perfil clicar em 'Seguindo'. Desça a barra de rolagem ao máximo que puder para o bot "visualizar" os usernames que vai deixar de seguir
-6. Clique na tecla 'F2' do teclado localizada na parte superior
-7. Ao abrir a mensagem em pop-up, clique em 'Ok' para o bot começar a funcionar
-8. Para o bot parar de funcionar, atualize a página dando 'F5' ou aguarde até chegar ao limite do contador em que apresentará um pop-up
-9. Para configurar as pessoas que o bot não vai deixar de seguir, abra o arquivo 'content.js' no editor de código da sua preferência (ou 'Bloco de notas')
-10. Procure pela váriavel 'usernamesPermitidos' e inclua dentro do colchete o username das pessoas que o bot vai ignorar, ou seja, vai continuar seguindo
-11. Em 'contadorUnfollow', defina o limite de contas que o bot vai deixar de seguir
-12. Salve o arquivo e clique em 'Recarregar' na extensão do navegador
-13. Atualize a página do Instagram e pronto. O bot está atualizado.
+5. Abrir o instagram e atualizar a página, clicando na tecla 'F5' localizada na parte superior do teclado
+6. Na aba do perfil do instagram, clicar em 'Seguindo', ao lado de 'Seguidores' (conforme a imagem abaixo)
+7. Clique na tecla 'F2'
+8. Ao abrir a mensagem em pop-up, insira a quantidade de seguidores que o bot vai deixar de seguir e clique em 'Ok' ou aperte 'Enter'
+9. Insira quais usernames não é para o bot deixar de seguir e clique em 'Ok' ou aperte 'Enter'
+9. O bot vai parar o funcionamento automaticamente ao atingir o limite ou quando sair da tela de funcionamento. Só vai voltar o funcionamento quando clicar em 'F2' novamente
+10. Não há necessidade de utilizar o editor de código para configurar o bot, mas se quiser contribuir realizando implementações, fique a vontade. Não se esqueça de dar uma estrela ao projeto
 
 ![Visualização no navegador Edge da extensão instagramUnfollow](image/extensaoEdge.png)
+<img src="image/printSeguindo.png" alt="Visualização das pessoas que estão seguindo no Instagram" width="300px">
 
-## Observação
+## Observações
 
-- O grupo Meta (Facebook, Instagram e Whatsapp) tem restrições com o uso de bots, então procure evitar deixar de seguir muitas pessoas na mesma hora
-- Deixe a página do instagram aberta enquanto o bot está sendo executado, pois caso contrário, o bot pode não funcionar bem
+- O grupo Meta (facebook, instagram, whatsapp, messenger, etc.) tem restrições com o uso de bots, então procure evitar deixar de seguir muitas pessoas na mesma hora
+- Deixe a página do instagram aberta enquanto o bot estiver em funcionamento, pois caso contrário, o bot pode não funcionar corretamente
+- Não tem problema desligar o monitor enquanto o bot estiver em funcionamento, mas configure a economia de energia do seu PC para que a CPU não desligue sozinha por inatividade
 - O bot foi testado em um sistema operacional Windows 10 Pro
+- O idioma do instagram que o bot foi testado é o Português (Brasil). O bot pode não funcionar corretamente em outro idioma
 
 ## Contato
 
--- content.js
+++ content.js
@@ -1,44 +1,62 @@
     // Detalhes importantes: Evitar utilizar o jQuery para referenciar os elementos e sim utilizar as propriedades do javascript vanilla mesmo
-    //Exemplo: ao invés de $("[class='algumaCoisa']") utilizar document.querySelectorAll(".")
+    // Exemplo: ao invés de $("[class='algumaCoisa']") utilizar document.querySelectorAll(".algumaCoisa")
+    // O motivo é que as extensões estão apresentando mau funcionamento com o framework jQuery
 document.addEventListener("keydown", function(event) {
+    var standardMessage = "\n\nSupport: jvnogueira2010@gmail.com";
     if (event.keyCode === 113) {
-var question = confirm("Pressione 'Ok' para o bot começar a funcionar")
-    }
-    if(question == true){
+        var question = prompt("Digite no campo abaixo quantas pessoas quer que o bot deixe de seguir:*"+
+        standardMessage)
+    };
+    if(question > 0){
+        var questionUsernames = prompt("Digite no campo abaixo quais perfis quer que o bot ignore, ou seja, quer que permaneça seguindo.\n\n"+
+        "Exemplo: 'username1', 'username2', 'username3'\n\n"+
+        "Preste atenção aos detalhes da vírgula e das aspas, caso contrário, o bot não vai funcionar corretamente."+
+        standardMessage)
         var i=0
         var contadorUnfollow=0
         percorrer()
         function percorrer(){
             var profileReference = document.querySelectorAll('._aano')[0].children[0].children[0].children[i] // Referenciar cada perfil da lista
             var getUsername = profileReference.children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML // Pegar o nome de usuário da lista
-            var usernamesPermitidos = ['exemplo1', 'exemplo2', 'exemplo3']
-        if(!usernamesPermitidos.includes(getUsername)){
-            if(i<500){  // Limite da lista de usernames que vão ser percorridas (não necessariamente o bot vai deixar de seguir, conforme a variável 'usernamePermitidos')
-                var limiteContador = 20;
+            var usernamesPrompt = questionUsernames.slice(1, -1).split("', '")
+            var usernamesPermitidos = [usernamesPrompt][0]
+            var limiteContador = question;
+            var buttonUnfollow = profileReference.children[0].children[0].children[0].children[2].children[0].children[0] // Botão para deixar de seguir  
+            if(!usernamesPermitidos.includes(getUsername)){
                 if(contadorUnfollow < limiteContador){ // Limite de usernames que o bot vai deixar de seguir
-                setTimeout(() => {
-                var buttonUnfollow = profileReference.children[0].children[0].children[0].children[2].children[0].children[0] // Botão para deixar de seguir  
-                buttonUnfollow.scrollIntoView() // Este método rola a página para que o elemento fique vísivel 
-                buttonUnfollow.click()
+                    setTimeout(() => {
+                        buttonUnfollow.scrollIntoView() // Este método rola a página para que o elemento fique vísivel 
+                        buttonUnfollow.click()
                         setTimeout(() => {
-                            [...document.querySelectorAll('button')].find(el => el.textContent == "Deixar de seguir").click()
+                            [...document.querySelectorAll('button')].find(el => el.textContent == "Deixar de seguir").click() // As duas opções são: 'Deixar de seguir' ou 'Cancelar'
                             contadorUnfollow++
-                            },3000)
-
+                        },3000)
                         setTimeout(() => {
                             i++
                             percorrer()
-                            },5000)
                         },5000)
-                    }else{
-                        alert('Chegou ao limite de '+limiteContador+' pessoa(s) que o bot deixou de seguir!\n\n'+
-                        'Para mudar isso, acesse o código-fonte e altere a váriavel "limiteContador" ou se preferir entre em contato pelo e-mail jvnogueira2010@gmail.com.')
-                    };
-                }
+                    },5000)
+                }else{
+                    alert('Chegou ao limite de '+limiteContador+' pessoa(s) que o bot deixou de seguir!'+
+                    standardMessage)
+                };
             }else{
                 i++
-                percorrer()
+                setTimeout(() => {
+                    profileReference.scrollIntoView()
+                    percorrer()
+                },1500)
             };
         }
+    }else if(typeof question == 'string' && question != 0){
+        alert('Digite apenas números!'+
+        standardMessage)
+    }else if(question == 0){
+        alert('Digite um número acima de 0!'+
+        standardMessage)
     }
 });
+
+// Próximo passo 1: Implementar pop-up de erro
+
+
