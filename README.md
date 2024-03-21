f3a394faa30cc1ccef75100129c871ab21af3b6e
João Vitor Nogueira <jvnogueira2010@gmail.com>
2024-03-21 01:29:21 -0300

    Remoção da biblioteca jQuery e inclusão do prompt para intervalo de tempo entre cada ciclo de unfollow

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Instagram Unfollow Bot
 
-Desenvolvimento de bot personalizado para deixar de seguir automaticamente na plataforma do instagram, tendo a funcionalidade de definir quais perfis para o bot permanecer seguindo e o limite de perfis para o bot deixar de seguir. Não há necessidade de conhecimento em programação para o download da extensão e ativação do bot
+Projeto de bot personalizado para deixar de seguir automaticamente na plataforma do instagram, tendo diversas funcionalidades. Vale enfatizar que não há necessidade de conhecimento em programação para o download da extensão e ativação do bot
 
 ## Funcionamento
 
@@ -17,10 +17,11 @@ Desenvolvimento de bot personalizado para deixar de seguir automaticamente na pl
 2. Na aba do perfil do instagram, clicar em 'Seguindo' ou 'Seguidores' (conforme a imagem abaixo)
 3. Após a página estar totalmente carregada, clique na tecla 'F2'
 4. Ao abrir a mensagem de pop-up (conforme a imagem abaixo), insira a quantidade de seguidores que o bot vai 'Deixar de seguir' ou 'Remover' e clique em 'Ok' ou aperte 'Enter'
-5. Insira quais usernames para o bot evitar e clique em 'Ok' ou aperte 'Enter'. Uma dica para ajudar na organização dos usernames, utilizar o bot que pega todos os usernames que o perfil do instagram está seguindo para colocar em uma planilha, segue o link do projeto: [www.github.com/jv-nogueira/getInstagramUsernameAll.com](https://github.com/jv-nogueira/getInstagramUsernameAll.git)
-6. O bot vai parar o funcionamento automaticamente nesses casos: ao atingir o limite de usernames que deixou de seguir ou removeu, quando sair da tela de funcionamento ou quando o bot percorrer todos os perfis. O bot vai voltar a funcionar quando clicar em 'F2' novamente
-7. Ao atingir o limite de usernames, então vai aparecer a mensagem em pop-up perguntando se deseja realizar o download do arquivo (unfollowList.txt) que contêm o nome e username que o bot deixou de seguir ou removeu. É uma ótima maneira para revisar os perfis. Para quem quer algo ainda mais organizado, o bloco de notas em txt já vai estar com os dados espaçados, permitindo que ao copiar e colar em uma planilha fiquem organizados em colunas "Nome" e "Username"
-8. Não há necessidade de utilizar o editor de código para configurar o bot, mas quem estiver interessado em contribuir realizando implementações, fique a vontade. Não se esqueça de dar uma estrela ao projeto
+5. Insira quantos segundos para durar cada ciclo de intervalo de tempo entre cada unfollow
+6. Insira quais usernames para o bot evitar o unfollow e clique em 'Ok' ou aperte 'Enter'
+7. O bot vai parar o funcionamento automaticamente nesses casos: ao atingir o limite de usernames que deixou de seguir ou removeu, quando sair da tela de funcionamento ou quando o bot percorrer todos os perfis. O bot vai voltar a funcionar quando clicar em 'F2' novamente
+8. Ao atingir o limite de usernames, então vai aparecer a mensagem em pop-up perguntando se deseja realizar o download do arquivo (unfollowList.txt) que contêm o nome e username que o bot deixou de seguir ou removeu. É uma ótima maneira para revisar os perfis. Para quem quer algo ainda mais organizado, o bloco de notas em txt já vai estar com os dados espaçados, permitindo que ao copiar e colar em uma planilha fiquem organizados em colunas "Nome" e "Username"
+9. Não há necessidade de utilizar o editor de código para configurar o bot, mas quem estiver interessado em contribuir realizando implementações, fique a vontade. Não se esqueça de dar uma estrela ao projeto
 
 <p float="flex">
   <img src="image/printSeguindo.png" alt="Visualização das pessoas que estão seguindo no Instagram" height="250px">
@@ -30,12 +31,20 @@ Desenvolvimento de bot personalizado para deixar de seguir automaticamente na pl
 
 ## Observações
 
-- O grupo Meta (facebook, instagram, whatsapp, messenger, etc.) tem restrições com o uso de bots, então use por conta e risco. Uma recomendação seria evitar deixar de seguir muitas pessoas na mesma hora
+- O grupo Meta (facebook, instagram, whatsapp, messenger, etc.) tem restrições com o uso de bots, então use por conta e risco. 
+- Uma recomendação, seria evitar deixar de seguir muitas pessoas na mesma hora. 
+  - No máximo um unfollow a cada 60 segundos (60 unfollow por hora)
+  - No máximo 200 unfollow por dia
+  - Para saber mais sobre isso:
+    - [clique aqui](https://portalpopclub.com.br/redes-sociais/limites-instagram-por-dia/)
+    - [e aqui](https://www.pathsocial.com/pt/resources/spring-clean-how-many-people-can-you-unfollow-on-instagram/)
 - O bot foi criado com o objetivo de diminuir as ações manuais, melhorar a organização e aumentar o tempo livre. Ou seja, sem objetivos maliciosos
 - Deixe a página do instagram ativa enquanto o bot estiver em funcionamento, pois caso contrário, o bot pode não funcionar corretamente
 - Não tem problema desligar o monitor enquanto o bot estiver em funcionamento, mas configure a economia de energia do seu PC para que a CPU não desligue sozinha por inatividade
 - O bot foi testado em um sistema operacional Windows 10 Pro
 - O idioma do instagram que o bot foi testado é o Português (Brasil). O bot pode não funcionar corretamente em outro idioma
+- Uma dica para ajudar na organização dos usernames é utilizar o bot que pega todos os usernames que o perfil do instagram está seguindo para salvar em uma planilha ou bloco de notas, segue o link do projeto: [www.github.com/jv-nogueira/getInstagramUsernameAll.com](https://github.com/jv-nogueira/getInstagramUsernameAll.git)
+- O bot foi desenvolvido com uma variação entre cada ciclo dos intervalos de tempo para assim se aproximar com o comportamento humano
 
 ## Contato
 
--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-    // Detalhes importantes: Evitar utilizar o jQuery para referenciar os elementos e sim utilizar as propriedades do javascript vanilla mesmo
-    // Exemplo: ao invés de $("[class='algumaCoisa']") utilizar document.querySelectorAll(".algumaCoisa")
-    // O motivo é que as extensões estão apresentando mau funcionamento com o framework jQuery
-
 document.addEventListener("keydown", function(event) {
     var standardMessage = "\n\nSupport: jvnogueira2010@gmail.com";
     if(document.location.href.includes('followers')||document.location.href.includes('following')){
@@ -16,115 +12,127 @@ document.addEventListener("keydown", function(event) {
         }
     };
     if(question > 0){
-        var questionUsernames = prompt("Digite no campo abaixo quais perfis quer que o bot ignore, ou seja, quer que permaneça seguindo.\n\n"+
-        "Exemplo: 'username1', 'username2', 'username3'\n\n"+
-        "Preste atenção aos detalhes da vírgula e das aspas, caso contrário, o bot não vai funcionar corretamente."+
+        var questionTemp = prompt("Digite no campo abaixo quantos segundos quer que dure cada ciclo de intervalo de tempo para o unfollow. Lembrando que quanto maior o intervalo de tempo, mais seguidores o bot vai "+promptUnfollow+" sem ser impedido pelo instagram."+
         standardMessage)
-        var i=0
-        var contadorUnfollow=0
-        var arrayArmazenarNameAndUsername = [];
-        percorrer()
-        function percorrer(){
-            var profileReference = document.querySelectorAll('._aano')[0].children[0].children[0].children[i] // Referenciar cada perfil da lista
-            var lastIndex = document.querySelectorAll('._aano')[0].children[0].children[0].children.length -1 // Referencia o last index da lista de perfis
-            var lastElementIndex = document.querySelectorAll('._aano')[0].children[0].children[0].children[lastIndex] // Visualiza o last element do index da lista de perfis
-            var viewProfile = profileReference.scrollIntoView() // Rola a página para que o elemento fique vísivel 
-            var getUsernameOrName = profileReference.children[0].children[0].children[0].children[1].children[0].children[0]
-            var getName = getUsernameOrName.children[1].children[0].innerText // Pegar o nome da lista
-            var getUsername = getUsernameOrName.children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML // Pegar o username da lista
-            var getUsernameAndName = {getName, getUsername}
-            var limiteContador = question;
-            var buttonUnfollow = profileReference.children[0].children[0].children[0].children[2].children[0].children[0] // Botão para deixar de seguir 
-            if(questionUsernames != null){
-                var usernamesPrompt = questionUsernames.slice(1, -1).split("', '")
-                var usernamesPermitidos = [usernamesPrompt][0]
-                if(!usernamesPermitidos.includes(getUsername)){
-                    viewProfile
-                    if(contadorUnfollow < limiteContador){ // Limite de usernames que o bot vai deixar de seguir
-                        arrayArmazenarNameAndUsername.push(getUsernameAndName)
-                        setTimeout(() => {
-                            buttonUnfollow.click()
+        if(!isNaN(questionTemp) && questionTemp > 10){
+            var answerTemp = questionTemp/2 // Pega a resposta do prompt e divide por 2 para dividir igualmente os segundos entre os intervalos
+            var questionUsernames = prompt("Digite no campo abaixo quais perfis quer que o bot ignore, ou seja, quer que permaneça seguindo.\n\n"+
+            "Exemplo: 'username1', 'username2', 'username3'\n\n"+
+            "Preste atenção aos detalhes da vírgula e das aspas, caso contrário, o bot não vai funcionar corretamente."+
+            standardMessage)
+            var i=0
+            var contadorUnfollow=0
+            var arrayArmazenarNameAndUsername = [];
+            percorrer()
+            function percorrer(){
+                var profileReference = document.querySelectorAll('._aano')[0].children[0].children[0].children[i] // Referenciar cada perfil da lista
+                if(i>0){
+                    var viewProfile = document.querySelectorAll('._aano')[0].children[0].children[0].children[i-1].scrollIntoView() // Rola a página para que o elemento fique vísivel 
+                };
+                var lastIndex = document.querySelectorAll('._aano')[0].children[0].children[0].children.length -1 // Referencia o last index da lista de perfis
+                var lastElementIndex = document.querySelectorAll('._aano')[0].children[0].children[0].children[lastIndex] // Visualiza o last element do index da lista de perfis
+                var getUsernameOrName = profileReference.children[0].children[0].children[0].children[1].children[0].children[0]
+                var getName = getUsernameOrName.children[1].children[0].innerText // Pegar o nome da lista
+                var getUsername = getUsernameOrName.children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML // Pegar o username da lista
+                var getUsernameAndName = {getName, getUsername}
+                var limiteContador = question;
+                var buttonUnfollow = profileReference.children[0].children[0].children[0].children[2].children[0].children[0] // Botão para deixar de seguir 
+                if(questionUsernames != null){
+                    var usernamesPrompt = questionUsernames.slice(1, -1).split("', '")
+                    var usernamesPermitidos = [usernamesPrompt][0]
+                    if(!usernamesPermitidos.includes(getUsername)){
+                            viewProfile 
+                        if(contadorUnfollow < limiteContador){ // Limite de usernames que o bot vai deixar de seguir
+                            arrayArmazenarNameAndUsername.push(getUsernameAndName)
                             setTimeout(() => {
-                                if(document.location.href.includes('followers')){
-                                    var buttonFollowingOrFollowers = 'Remover' // As duas opções são: 'Remover' ou 'Cancelar'
-                                }else if(document.location.href.includes('following')){
-                                    var buttonFollowingOrFollowers = 'Deixar de seguir' // As duas opções são: 'Deixar de seguir' ou 'Cancelar'
-                                };
-                                [...document.querySelectorAll('button')].find(el => el.textContent == buttonFollowingOrFollowers).click()
-                                contadorUnfollow++
-                            },getRandomSeconds(2))
-                            setTimeout(() => {
-                                i++
-                                percorrer()
-                            },getRandomSeconds(4))
-                        },getRandomSeconds(4))
-                    }else{
-                        var removeWhiteSpace = limiteContador.replace(/\s/g, '')
-                        var singular = "" 
-                        var plural = ""
-                        if(limiteContador > 1){
-                            plural = "s"
+                                buttonUnfollow.click()
+                                setTimeout(() => {
+                                    if(document.location.href.includes('followers')){
+                                        var buttonFollowingOrFollowers = 'Remover' // As duas opções são: 'Remover' ou 'Cancelar'
+                                    }else if(document.location.href.includes('following')){
+                                        var buttonFollowingOrFollowers = 'Deixar de seguir' // As duas opções são: 'Deixar de seguir' ou 'Cancelar'
+                                    };
+                                    [...document.querySelectorAll('button')].find(el => el.textContent == buttonFollowingOrFollowers).click()
+                                    contadorUnfollow++
+                                    i++
+                                    percorrer()
+                                },getRandomSeconds(answerTemp))
+                            },getRandomSeconds(answerTemp))
                         }else{
-                            singular = ""
+                            setTimeout(() => {
+                            var removeWhiteSpace = limiteContador.replace(/\s/g, '')
+                            var singular = "" 
+                            var plural = ""
+                            if(limiteContador > 1){
+                                plural = "s"
+                            }else{
+                                singular = ""
+                            };
+                            if(document.location.href.includes('followers')){
+                                var phraseUnfollow = "removeu dos seguidores"
+                            }else if(document.location.href.includes('following')){
+                                var phraseUnfollow = "deixou de seguir"
+                            }
+                            var questionFinal = confirm("Chegou ao limite de "+removeWhiteSpace+" pessoa"+singular+plural+" que o bot "+phraseUnfollow+"!\n\n"+
+                            "Deseja a lista com as pessoas que o bot "+phraseUnfollow+"? Se sim, clique em 'Ok' ou aperte 'Enter' para o download do arquivo 'unfollowList.txt'"+
+                            standardMessage)
+                            if(questionFinal == true){
+                                // Criar o conteúdo do CSV
+                                var csvContent = "\uFEFFNome\tUsername\n";
+                                arrayArmazenarNameAndUsername.forEach(function (item) {
+                                csvContent += item.getName + "\t" + item.getUsername + "\n";
+                                });
+
+                                // Criar um Blob com o conteúdo do CSV
+                                var blob = new Blob([csvContent], { type: "text/txt" });
+
+                                // Criar um link para o Blob
+                                var link = document.createElement("a");
+                                link.href = window.URL.createObjectURL(blob);
+
+                                // Definir o nome do arquivo
+                                link.download = "unfollowList.txt";
+
+                                // Adicionar o link à página e clicar automaticamente para iniciar o download
+                                document.body.appendChild(link);
+                                link.click();
+
+                                // Remover o link da página
+                                document.body.removeChild(link);
+                            }
+                            },getRandomSeconds(1))
                         };
-                        if(document.location.href.includes('followers')){
-                            var phraseUnfollow = "removeu dos seguidores"
-                        }else if(document.location.href.includes('following')){
-                            var phraseUnfollow = "deixou de seguir"
-                        }
-                        var questionFinal = confirm("Chegou ao limite de "+removeWhiteSpace+" pessoa"+singular+plural+" que o bot "+phraseUnfollow+"!\n\n"+
-                        "Deseja a lista com as pessoas que o bot "+phraseUnfollow+"? Se sim, clique em 'Ok' ou aperte 'Enter' para o download do arquivo 'unfollowList.txt'"+
-                        standardMessage)
-                        if(questionFinal == true){
-                            // Criar o conteúdo do CSV
-                            var csvContent = "\uFEFFNome\tUsername\n";
-                            arrayArmazenarNameAndUsername.forEach(function (item) {
-                            csvContent += item.getName + "\t" + item.getUsername + "\n";
-                            });
-
-                            // Criar um Blob com o conteúdo do CSV
-                            var blob = new Blob([csvContent], { type: "text/txt" });
-
-                            // Criar um link para o Blob
-                            var link = document.createElement("a");
-                            link.href = window.URL.createObjectURL(blob);
-
-                            // Definir o nome do arquivo
-                            link.download = "unfollowList.txt";
-
-                            // Adicionar o link à página e clicar automaticamente para iniciar o download
-                            document.body.appendChild(link);
-                            link.click();
-
-                            // Remover o link da página
-                            document.body.removeChild(link);
-                        }
-                    };
-                }else if(i < lastIndex){
-                    i++
-                    percorrer()
-                }else{
-                    setTimeout(() => {
-                        lastElementIndex.scrollIntoView()
+                    }else if(i < lastIndex){
+                        i++
+                        percorrer()
+                    }else{
                         setTimeout(() => {
-                            percorrer()
+                            lastElementIndex.scrollIntoView()
+                            setTimeout(() => {
+                                percorrer()
+                            },getRandomSeconds(1))
                         },getRandomSeconds(1))
-                    },getRandomSeconds(1))
+                    }
                 }
             }
-        }
-    }else if(typeof question == 'string' && question != 0){
-        alert('Digite apenas números!'+
-        standardMessage)
-    }else if(question == 0){
-        alert('Digite um número acima de 0!'+
-        standardMessage)
-    } 
+        }else if(questionTemp <= 10){
+            var numberTen = alert('Digite um número acima de 10!'+
+            standardMessage)
+        }else if(typeof questionTemp == 'string'){
+            var withoutString = alert('Digite apenas números!'+
+            standardMessage)
+        }; 
+        }else if(typeof question == 'string' && question != 0){
+            var numberOnly = alert('Digite apenas números!'+
+            standardMessage)
+        }else if(question == 0){
+            var numberZero = alert('Digite um número acima de 0!'+
+            standardMessage)
+        }; 
 
-
-    function getRandomSeconds(sum){
-        let sec222 = Math.random() * 2 + sum; // Multiplica um random number e soma 
-        sec222 =  parseFloat(sec222.toFixed(3))*1000; // Diminui para 3 casas decimais, converte de string para number e multiplica por 1000 que corresponde a 1 segundo
-        return sec222; // retorna o resultado aleatorio em segundos 
-    }
+function getRandomSeconds(sum){
+    let sec222 = Math.random() * 4 + sum; // Multiplica um random number e soma 
+    sec222 =  parseFloat(sec222.toFixed(3))*1000; // Diminui para 3 casas decimais, converte de string para number e multiplica por 1000 que corresponde a 1 segundo
+    return sec222; // retorna o resultado aleatorio em segundos 
+};
 });
